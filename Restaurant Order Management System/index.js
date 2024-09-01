1a637a0de627478c4f71799b30fe2c0e342924eb
PromojitPaul <promojitpaul@gmail.com>
2024-09-01 12:37:04 +0530

    Add files via upload

--- Restaurant Order Management System/index.js
+++ Restaurant Order Management System/index.js
@@ -3,13 +3,13 @@ const Restaurant = require('./restaurant');
 const myRestaurant = new Restaurant();
 
 // Adding menu items
-myRestaurant.addMenuItem("Biriyani", 250);
-myRestaurant.addMenuItem("Pakoda", 20);
-myRestaurant.addMenuItem("Coca-cola", 50);
+myRestaurant.addMenuItem("Biryani", 250);
+myRestaurant.addMenuItem("Pakora", 20);
+myRestaurant.addMenuItem("Coca-Cola", 50);
 
 // Taking orders
-myRestaurant.takeOrder(1, ["Biriyani", "Pakoda"]);
-myRestaurant.takeOrder(2, ["Coca-cola"]);
+myRestaurant.takeOrder(1, ["Biryani", "Pakora"]);
+myRestaurant.takeOrder(2, ["Coca-Cola"]);
 
 // Updating order status
 myRestaurant.updateOrderStatus(1, "Served");
